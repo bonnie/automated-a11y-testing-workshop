70e46077d5cb76b2d48ecf7d38d29864c5dce140
Bonnie Schulkin <biz@bonnieschulkin.com>
2021-11-17 10:09:46 -0800

    Ignore cypress directory for jest

--- chakra-ui/jest.config.ts
+++ chakra-ui/jest.config.ts
@@ -1,5 +1,6 @@
 export default {
   testEnvironment: "jsdom",
+  testPathIgnorePatterns: ["/cypress/"],
   setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
   moduleNameMapper: {
     "\\.(css|less)$": "identity-obj-proxy",
