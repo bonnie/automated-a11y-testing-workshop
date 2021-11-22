85a32bb6395613cdb41aa939de15f863f89b8f16
Bonnie Schulkin <biz@bonnieschulkin.com>
2021-11-21 21:12:13 -0800

    Update import path

--- copy-paste-files/App.test.jsx
+++ copy-paste-files/App.test.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import { axe, toHaveNoViolations } from "jest-axe";
-import App from "../chakra-ui/src/App";
+import App from "./App";
 
 expect.extend(toHaveNoViolations);
 
